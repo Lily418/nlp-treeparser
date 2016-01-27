a25eb3bb3e029086ad221dd09f5dbced4010971f
Lily Hoskin <lily@memrica.com>
2016-01-27 17:49:34 +0000

    Move to older version of nlp compromise, create dist directory

--- nlp/sentenceToTree.js
+++ nlp/sentenceToTree.js
@@ -1,6 +1,6 @@
 var LSU = require('./lsu.js');
 var nlp_compromise = require("nlp_compromise");
-
+console.log(nlp_compromise)
 var sentenceToTree = function(sentence) {
   //Split sentence into a list of words
   var taggedWords = nlp_compromise.text(sentence).terms().map(function(term){
